adf05e3c5cceeca7014b1a6cc70d3eea1608ebc9
Mila Neves <mila.neves000@gmail.com>
2025-03-25 18:18:07 -0300

    added bogus package and fluent assertions

---
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,19 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Comunication.Requests;
+
+namespace Validators.Test.User.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void WhenRegisterUserIsSuccess()
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build();
+            var result = validator.Validate(request);
+           result.IsValid.Should().BeTrue();
+        }
+    }
+}
