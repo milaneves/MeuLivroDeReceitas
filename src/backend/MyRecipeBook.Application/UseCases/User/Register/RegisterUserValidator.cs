47f504044bfbad4fb2085fef188cda2dffd944cf
Mila Neves <mila.neves000@gmail.com>
2025-03-26 18:48:47 -0300

    feat: removed package fluentAssertion and added packageShouldly

--- src/backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -8,8 +8,11 @@ namespace MyRecipeBook.Application.UseCases.User.Register
         {
             RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessageException.NAME_EMPTY);
             RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessageException.EMAIL_EMPTY);
-            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessageException.EMAIL_INVALID);
             RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
+            When(user => string.IsNullOrEmpty(user.Email) == false, () =>
+            {
+                RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessageException.EMAIL_INVALID);
+            });
         }
     }
 }
--- tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -1,7 +1,7 @@
 ﻿using CommonTestUtilities.Requests;
-using FluentAssertions;
 using MyRecipeBook.Application.UseCases.User.Register;
-using MyRecipeBook.Comunication.Requests;
+using MyRecipeBook.Exception;
+using Shouldly;
 
 namespace Validators.Test.User.Register
 {
@@ -13,7 +13,41 @@ namespace Validators.Test.User.Register
             var validator = new RegisterUserValidator();
             var request = RequestRegisterUserJsonBuilder.Build();
             var result = validator.Validate(request);
-           result.IsValid.Should().BeTrue();
+            result.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void Error_Name_Empty()
+        {   
+            //Arrange
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+            
+            //Act
+            var result = validator.Validate(request);
+            
+            //Assert
+            result.IsValid.ShouldBeFalse();
+            result.Errors
+                .ShouldContain(x => x.ErrorMessage.Equals(ResourceMessageException.NAME_EMPTY));
+        }
+
+        [Fact]
+        public void Error_Email_Empty()
+        {
+            //Arrange
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Email = string.Empty;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.ShouldBeFalse();
+            result.Errors
+                .ShouldContain(x => x.ErrorMessage.Equals(ResourceMessageException.EMAIL_EMPTY));
         }
     }
 }
