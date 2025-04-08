622b97d3f791999364f75057a068e9ea8364223b
Mila Neves <mila.neves000@gmail.com>
2025-04-08 16:37:41 -0300

    feat: Add integration test

--- tests/WebApi.Test/User/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -1,9 +1,11 @@
 ﻿using CommonTestUtilities.Requests;
-using Microsoft.AspNetCore.Mvc.Testing;
+using MyRecipeBook.Exception;
 using Shouldly;
+using System.Globalization;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
+using WebApi.Test.InlineData;
 
 namespace WebApi.Test.User.Register
 {
@@ -26,6 +28,30 @@ namespace WebApi.Test.User.Register
             await using var responseBody = await response.Content.ReadAsStreamAsync();
             var responseData = await JsonDocument.ParseAsync(responseBody);
             responseData.RootElement.GetProperty("name").GetString().ShouldNotBeNullOrWhiteSpace();
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task Error_Empty_Name(string culture)
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+            if (_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
+                _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
+
+            _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
+            var response = await _httpClient.PostAsJsonAsync("User", request);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+            var expectedMessage  = ResourceMessagesException.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(culture));
+
+            errors.ShouldBeUnique();
+            errors.ShouldContain(error => error.GetString()!.Equals(expectedMessage));
+
         }
     }
 }
