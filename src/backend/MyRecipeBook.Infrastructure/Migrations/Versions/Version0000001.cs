5fea38db5bc4ae307666607810aaf06f77564d84
Mila Neves <mila.neves000@gmail.com>
2025-10-23 17:39:14 -0300

    feat: add migrations recipes

--- src/backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version0000001.cs
+++ src/backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -3,11 +3,11 @@
 namespace MyRecipeBook.Infrastructure.Migrations.Versions
 {
     [Migration(DatabaseVersions.TABLE_USER,"Create table to save the user's information")]
-    public class Version0000001 : ForwardOnlyMigration
+    public class Version0000001 : VersionBase
     {
         public override void Up()
         {
-            Create.Table("Users")
+            CreateTable("Users")
                  .WithColumn("Name").AsString(255).NotNullable()
                  .WithColumn("Email").AsString(255).NotNullable()
                  .WithColumn("Password").AsString(2000).NotNullable()
